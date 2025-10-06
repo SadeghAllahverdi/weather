711abe76ca72b6a527997754cbcaa3fad3d1c401
SadeghAllahverdi <sadegh7644@gmail.com>
2025-10-06 19:59:52 +0200

    LiquidGlass plus Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import "./App.css";
+import LiquidGlass from "liquid-glass-react";
+
+//import CurrentWeather from "./components/CurrentWeather";
+// import DailyWeather from "./components/DailyWeather";
+// import HourlyWeather from "./components/HourlyWeather";
+// import SearchBar from "./components/SearchBar";
+// import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const containerRef = useRef<HTMLDivElement>(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>CI/CD change</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      ref={containerRef}
+      className="bg-linear-to-bl from-cyan-500 to-blue-500 w-full h-screen bg-image flex flex-col gap-6 p-8 mx-auto"
+    >
+      <LiquidGlass
+        mouseContainer={containerRef}
+        elasticity={0.3}
+        style={{ position: "fixed", top: "50%", left: "50%" }}
+        cornerRadius={10}
+        className="bg-linear-to-r from-blue-50 to-red-200"
+      >
+        <div className="p-6">
+          <h2>Glass responds to mouse anywhere in the container</h2>
+        </div>
+      </LiquidGlass>
+
+      <LiquidGlass
+        mouseContainer={containerRef}
+        elasticity={1.0}
+        style={{ position: "fixed", top: "10%", left: "50%" }}
+      >
+        <div className="p-6 ">
+          <h2>Glass responds to mouse anywhere in the container</h2>
+        </div>
+      </LiquidGlass>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CurrentWeather.tsx
@@ -0,0 +1,8 @@
+export default function CurrentWeather() {
+  return (
+    <div className="p-6 text-center backdrop-blur-md bg-white/10 border border-white/20 rounded-2xl">
+      <h2 className="text-white text-2xl font-bold">CurrentWeather</h2>
+      <p className="text-white/90">This will have the liquid glass effect</p>
+    </div>
+  );
+}
