d3f7d01a3be98a536576c93ab8317fd68930de5e
SadeghAllahverdi <sadegh7644@gmail.com>
2025-10-07 11:17:52 +0200

    Addig initial glass effect structre

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,70 @@
 import { useRef } from "react";
-import "./App.css";
-import LiquidGlass from "liquid-glass-react";
 
-//import CurrentWeather from "./components/CurrentWeather";
-// import DailyWeather from "./components/DailyWeather";
-// import HourlyWeather from "./components/HourlyWeather";
-// import SearchBar from "./components/SearchBar";
-// import Footer from "./components/Footer";
+import { LiquidGlass as NomalGlass } from "@liquidglass/react";
+import LiquidGlass from "liquid-glass-react";
 
 function App() {
   const containerRef = useRef<HTMLDivElement>(null);
-
   return (
-    <div
-      ref={containerRef}
-      className="bg-linear-to-bl from-cyan-500 to-blue-500 w-full h-screen bg-image flex flex-col gap-6 p-8 mx-auto"
-    >
-      <LiquidGlass
-        mouseContainer={containerRef}
-        elasticity={0.3}
-        style={{ position: "fixed", top: "50%", left: "50%" }}
-        cornerRadius={10}
-        className="bg-linear-to-r from-blue-50 to-red-200"
+    <div className="w-screen h-screen">
+      <div
+        className="w-full h-full 
+                      bg-[url(https://images.pexels.com/photos/956999/milky-way-starry-sky-night-sky-star-956999.jpeg?_gl=1*1r4qe7s*_ga*MTQyOTQzODc5NS4xNzU5Nzc3NTE2*_ga_8JE65Q40S6*czE3NTk4Mjg0MTckbzIkZzEkdDE3NTk4Mjg1MTYkajQyJGwwJGgw)] 
+                      bg-cover bg-center bg-no-repeat
+                      flex flex-col items-center justify-center gap-8 p-8"
       >
-        <div className="p-6">
-          <h2>Glass responds to mouse anywhere in the container</h2>
-        </div>
-      </LiquidGlass>
+        <LiquidGlass
+          mouseContainer={containerRef}
+          cornerRadius={20}
+          elasticity={0.7}
+          style={{ position: "fixed", top: "5%", left: "50%" }}
+        >
+          <div className="h-5 w-50">this tracks mouse</div>
+        </LiquidGlass>
 
-      <LiquidGlass
-        mouseContainer={containerRef}
-        elasticity={1.0}
-        style={{ position: "fixed", top: "10%", left: "50%" }}
-      >
-        <div className="p-6 ">
-          <h2>Glass responds to mouse anywhere in the container</h2>
+        <div className="w-full min-h-screen flex flex-col items-center justify-center gap-4">
+          <div className="w-full flex justify-between gap-4">
+            <div className="flex-1 h-84 rounded-2xl">
+              <NomalGlass
+                borderRadius={20}
+                blur={2}
+                contrast={1.15}
+                brightness={2}
+                saturation={1.1}
+                elasticity={0.3}
+              >
+                <div className="px-8 py-4 w-auto">this tracks mouse</div>
+              </NomalGlass>
+            </div>
+
+            <div className="w-2/5 h-84 rounded-2xl">
+              <NomalGlass
+                borderRadius={20}
+                blur={2}
+                contrast={1.15}
+                brightness={2}
+                saturation={1.1}
+                elasticity={0.2}
+              >
+                <div className="px-8 py-4 w-auto">this tracks mouse</div>
+              </NomalGlass>
+            </div>
+          </div>
+
+          <div className="w-full h-48 rounded-2xl">
+            <NomalGlass
+              borderRadius={20}
+              blur={1.8}
+              contrast={1.15}
+              brightness={2}
+              saturation={1.1}
+              elasticity={0.2}
+            >
+              <div className="px-8 py-4 w-auto">this tracks mouse</div>
+            </NomalGlass>
+          </div>
         </div>
-      </LiquidGlass>
+      </div>
     </div>
   );
 }
