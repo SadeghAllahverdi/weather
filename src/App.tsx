c744421c7c8797a433aae8918085725147e421bd
Roottoot <syuleiman@gmail.com>
2025-10-26 15:23:40 +0100

    Fixed screen size, Added dark mode, Fixed MObile view

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import CurrentWeather from "./components/CurrentWeather";
 import DailyWeather from "./components/DailyWeather";
 import HourlyWeather from "./components/HourlyWeather";
 
-function App() {
+export default function App() {
   const containerRef = useRef<HTMLDivElement>(null);
   const [weather, setWeather] = useState<WeatherData | null>(null);
   const handleSearch = async (cityName: string) => {
@@ -15,29 +15,22 @@ function App() {
     setWeather(weattherData);
   };
   return (
-    <div className="w-screen min-h-screen font-[Roboto_Serif]">
+    <div className="w-full min-h-screen font-[Roboto_Serif]
+                  bg-[url(https://img.craftpix.net/2022/12/Free-Mountain-Backgrounds-Pixel-Art6.png)] bg-center">
       <div
         ref={containerRef}
-        className="w-full min-h-screen
-          bg-[url(https://img.craftpix.net/2022/12/Free-Mountain-Backgrounds-Pixel-Art6.png))]
-          bg-cover bg-center
-          flex flex-col items-center justify-center gap-8 p-8"
-      >
-        {/* Search bar component */}
+        className="flex flex-col items-center justify-center gap-5 p-8 max-w-full">
         <SearchBar containerRef={containerRef} onSearch={handleSearch} />
-
         {/* Weather components */}
         <div className="w-8/10 min-h-screen z-0 flex flex-col items-center justify-center gap-7 mt-30 md:mt-20">
           <div className="w-full flex flex-col md:flex-row justify-between gap-7">
             <div className="w-full md:flex-1 h-96 md:h-84 rounded-2xl hover:scale-105 transition duration-500 ease-in-out">
               <CurrentWeather info={weather?.current} />
             </div>
-
             <div className="w-full md:w-2/5 h-64 md:h-84 rounded-2xl hover:scale-105 transition duration-500 ease-in-out">
               <DailyWeather info={weather?.daily} />
             </div>
           </div>
-
           <div className="w-full h-64 rounded-2xl hover:scale-105 transition duration-500 ease-in-out">
             <HourlyWeather info={weather?.hourly} />
           </div>
@@ -46,5 +39,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
