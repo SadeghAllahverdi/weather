8232fb73fa78b64b9cf52d01f3415ce0af394f14
Roottoot <syuleiman@gmail.com>
2025-12-18 17:22:27 +0100

    Change background image based on search and default weather info based on ip

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import getWeather from "./assets/api/WeatherApi.ts";
 import type { WeatherData } from "./assets/api/WeatherApi.ts";
 
@@ -6,21 +6,41 @@ import SearchBar from "./components/SearchBar";
 import CurrentWeather from "./components/CurrentWeather";
 import DailyWeather from "./components/DailyWeather";
 import HourlyWeather from "./components/HourlyWeather";
+import { cityBackgroundImage, getUserIp, ipToCity } from "./assets/api/InitialLocationGuess.ts";
 
 export default function App() {
+  const [backgroundImageUrl, setBackgroundImageUrl] = useState("/cologne-christmas-market-1600x890.jpg");
   const containerRef = useRef<HTMLDivElement>(null);
   const [weather, setWeather] = useState<WeatherData | null>(null);
   const handleSearch = async (cityName: string) => {
-    const weattherData = await getWeather(cityName);
-    setWeather(weattherData);
+    const weatherData = await getWeather(cityName);
+    setWeather(weatherData);
   };
+
+  useEffect(() => {
+    if (!weather) {
+      getUserIp().then((ip) => {
+        ipToCity(ip).then((city) => {
+          getWeather(city).then((weatherData) => {
+            setWeather(weatherData);
+          });
+          cityBackgroundImage(city).then((newImageUrl) => {
+            setBackgroundImageUrl(newImageUrl);
+          });
+        });
+      });
+    }
+  }, []);
+
   return (
     <div className="w-full min-h-screen font-[Roboto_Serif]
-                  bg-[url(https://img.craftpix.net/2022/12/Free-Mountain-Backgrounds-Pixel-Art6.png)] bg-center">
+                   bg-center bg-cover bg-no-repeat bg-fixed"
+      style={{ backgroundImage: `url(${backgroundImageUrl})` }}
+    >
       <div
         ref={containerRef}
         className="flex flex-col items-center justify-center gap-5 p-8 max-w-full">
-        <SearchBar containerRef={containerRef} onSearch={handleSearch} />
+        <SearchBar containerRef={containerRef} onSearch={handleSearch} setBackgroundImageUrl={setBackgroundImageUrl} />
         {/* Weather components */}
         <div className="w-8/10 min-h-screen z-0 flex flex-col items-center justify-center gap-7 mt-30 md:mt-20">
           <div className="w-full flex flex-col md:flex-row justify-between gap-7">
